57fd8305a7fc3aa9a323341fd2eab088ea9c7b6b
Unknown <pham.anh.viet.vlth@gmail.com>
2018-12-08 23:58:13 +0700

    snackbar anko kotlin

--- app/src/main/java/com/example/anhvietpham/anko_kotlin/ankodialogs/SnackBarActivity.kt
+++ app/src/main/java/com/example/anhvietpham/anko_kotlin/ankodialogs/SnackBarActivity.kt
@@ -1,4 +1,33 @@
 package com.example.anhvietpham.anko_kotlin.ankodialogs
 
-class SnackBarActivity {
+import android.graphics.Color
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.Gravity
+import android.widget.LinearLayout
+import org.jetbrains.anko.backgroundColor
+import org.jetbrains.anko.button
+import org.jetbrains.anko.design.coordinatorLayout
+import org.jetbrains.anko.design.snackbar
+import org.jetbrains.anko.linearLayout
+import org.jetbrains.anko.sdk25.coroutines.onClick
+
+class SnackBarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        coordinatorLayout {
+            backgroundColor = Color.parseColor("#f2f2f2")
+            linearLayout {
+                orientation = LinearLayout.VERTICAL
+                button {
+                    text = "Simple Snackbar"
+                    onClick{
+                        rootView.snackbar("Hello Anh Viet Pham")
+                    }
+                }
+            }.lparams {
+                gravity = Gravity.TOP or Gravity.CENTER_HORIZONTAL
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/anhvietpham/anko_kotlin/convertDSL/AnkoDSLActivity.kt
+++ app/src/main/java/com/example/anhvietpham/anko_kotlin/convertDSL/AnkoDSLActivity.kt
@@ -1,4 +1,12 @@
 package com.example.anhvietpham.anko_kotlin.convertDSL
 
-class AnkoDSLActivity {
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.example.anhvietpham.anko_kotlin.R
+
+class AnkoDSLActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
 }
